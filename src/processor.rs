491f0622444c3b60e278617ccf21774a6bd96db3
edanur-caglayann <caglayanedanur23@gmail.com>
2024-09-07 20:20:25 +0300

    programda duzenlemeler yapildi

--- src/processor.rs
+++ src/processor.rs
@@ -6,7 +6,7 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{ 
     account_info::{next_account_info, AccountInfo}, clock, config, entrypoint::ProgramResult, lamports, msg, program::{invoke, invoke_signed}, program_error::ProgramError, pubkey::{self, Pubkey}, rent::Rent, system_instruction::{self}, system_program, sysvar::Sysvar
     };
-    use crate::{instruction::RNGProgramInstruction, state::{GithubRepo, User}};
+    use crate::{instruction::RNGProgramInstruction, state::{GithubRepo, User, PrCount}};
     use crate::error::RNGProgramError::{InvalidInstruction};
     pub struct Processor;
     impl Processor {
@@ -36,39 +36,72 @@ use solana_program::{
         
         if !payer.is_signer{ 
           msg!("payer is not a signer");
-          // return Err(AuthorityError.into());
+          return Err(ProgramError::MissingRequiredSignature);
         }
         
-        let(user_pda_address, bump) = Pubkey::find_program_address(&[b"user_pda", payer.key.as_ref() ], program_id);
+         // PDA hesabı oluşturma
+       let (user_pda_address, bump) = Pubkey::find_program_address(
+      &[b"user_pda", github_username.as_bytes()], 
+      program_id
+         );
 
-        // min rent bedeli icin user structini serilestiririz
+         // 'user' hesabı PDA adresi ile aynı mı kontrol et
+        if &user_pda_address != user.key {
+      msg!("Provided user account does not match derived PDA.");
+      return Err(ProgramError::InvalidArgument);
+         }
+
+        let rent = Rent::default();
+        let user_rent = rent.minimum_balance(52);
+
+        invoke_signed ( 
+          &system_instruction::create_account(payer.key, &user_pda_address, user_rent,52, program_id),
+          &[user.clone(),payer.clone()],
+          &[
+            &[b"user_pda",github_username.as_bytes(), &[bump]]
+          ]
+          )?;
+          
+          
         let user_info = User {
           github_username,
           phantom_wallet,
-          total_pull_request_count:0,
-          pull_requests_per_repo: HashMap::new(),
-      };
+         };
 
-        let mut serialized_data = vec![];
-        user_info.serialize(&mut serialized_data)?;
-
-        let rent = Rent::default();
-        let user_rent = rent.minimum_balance(serialized_data.len());
-
-        invoke_signed ( 
-          &system_instruction::create_account(payer.key, &user_pda_address, user_rent,serialized_data.len() as u64, program_id),
-          &[user.clone(),payer.clone()],
-          &[
-            &[b"user_pda",payer.key.as_ref(), &[bump]]
-          ]
-          )?;
-    
         user_info.serialize(&mut &mut user.try_borrow_mut_data()?[..])?;
         
         Ok(())
       }
 
-      pub fn github_repo (
+      pub fn pull_request_count (
+        accounts: &[AccountInfo],
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let payer = next_account_info(account_info_iter)?;
+        let user = next_account_info(account_info_iter)?;
+  
+        let user_data = User::try_from_slice(&user.data.borrow())?;
+
+        let rent = Rent:: default();
+        let pr_count_rent = rent.minimum_balance(8);
+  
+        let(pr_counter_address, bump) = Pubkey::find_program_address(
+          &[b"pull request counter", &user_data.phantom_wallet], 
+          program_id);
+  
+        invoke_signed ( 
+          &system_instruction::create_account(payer.key, &pr_counter_address,pr_count_rent , 8, program_id),
+          &[user.clone(),payer.clone()],
+          &[
+            &[b"pull request counter",  &user_data.phantom_wallet, &[bump]]
+          ]
+        )?;
+
+        Ok(())
+      }
+
+      pub fn create_repo (
         accounts: &[AccountInfo],
         program_id: &Pubkey,
         data:GithubRepo,
@@ -81,22 +114,14 @@ use solana_program::{
           msg!("payer is not a signer");
           // return Err(AuthorityError.into());
         }
+        let repo_data = GithubRepo::try_from_slice(&github_repo_account.data.borrow())?;
         
-        let(repo_pda_address, bump) = Pubkey::find_program_address(&[b"repo_pda", payer.key.as_ref() ], program_id);
+        let(repo_pda_address, bump) = Pubkey::find_program_address(
+          &[b"repo_pda",repo_data.repo_url.as_bytes()], 
+          program_id);
 
-        let mut github_repo_data = GithubRepo::try_from_slice(&github_repo_account.data.borrow())?;
-       
-        let github_repo = GithubRepo {
-            repo_url:github_repo_data.repo_url,
-            total_pull_requests:github_repo_data.total_pull_requests,
-            pull_request_limit:github_repo_data.pull_request_limit,
-            reward_per_pull_request:github_repo_data.reward_per_pull_request,
-            owner_wallet_address:github_repo_data.owner_wallet_address,
-            pull_request_contributors: HashMap::new(),
-        };
-
-         let mut serialized_data = vec![];
-         github_repo.serialize(&mut serialized_data)?;
+          let mut serialized_data = vec![];
+          data.serialize(&mut serialized_data)?;
 
         let rent = Rent::default();
         let repo_rent = rent.minimum_balance(serialized_data.len());
@@ -105,46 +130,19 @@ use solana_program::{
           &system_instruction::create_account(payer.key, &repo_pda_address, repo_rent, serialized_data.len() as u64, program_id),
           &[github_repo_account.clone(),payer.clone()],
           &[
-            &[b"repo_pda", payer.key.as_ref(), &[bump]]
+            &[b"repo_pda",repo_data.repo_url.as_bytes(), &[bump]]
           ]
           )?;
 
-          github_repo.serialize(&mut &mut github_repo_account.try_borrow_mut_data()?[..])?;
+        // Veriyi yeni hesaba kaydet
+        let mut repo_data_account = github_repo_account.try_borrow_mut_data()?;
+
+        // Serileştirilmiş veriyi doğrudan kaydedin
+          repo_data_account[..serialized_data.len()].copy_from_slice(&serialized_data);
         
         Ok(())
       }
      
-      // pull request yapma fonks
-      pub fn make_pull_request (
-        accounts: &[AccountInfo],
-        program_id: &Pubkey,
-        repo_url: String,
-      ) -> ProgramResult {
-        let account_info_iter = &mut accounts.iter();
-        let payer = next_account_info(account_info_iter)?;
-        let user = next_account_info(account_info_iter)?;
-        let github_repo_account = next_account_info(account_info_iter)?;
-
-        if !payer.is_signer {
-          msg!("payer is not a signer");
-          return Err(ProgramError::MissingRequiredSignature);
-      }
-        let mut user_data = User::try_from_slice(&user.data.borrow())?;
-        let mut repo_data = GithubRepo::try_from_slice(&github_repo_account.data.borrow())?;
-
-         // Kullanıcının repo için pull request sayısını güncelleyelim
-        let user_repo_pull_requests = user_data.pull_requests_per_repo.entry(repo_url.clone()).or_insert(0);
-        *user_repo_pull_requests += 1;
-
-        // toplam pr sayisini guncelleyelim
-        user_data.total_pull_request_count = user_data.total_pull_request_count.checked_add(1).ok_or(InvalidInstruction)?;
-
-        user_data.serialize(&mut &mut user.try_borrow_mut_data()?[..])?;
-        repo_data.serialize(&mut &mut github_repo_account.try_borrow_mut_data()?[..])?;
-
-        Ok(())
-      }
-
       // odul transfer fonks
       pub fn transfer_reward (
         accounts: &[AccountInfo],
@@ -154,65 +152,79 @@ use solana_program::{
         let payer = next_account_info(account_info_iter)?;
         let user = next_account_info(account_info_iter)?;
         let github_repo_account = next_account_info(account_info_iter)?;
+        let pr_count = next_account_info(account_info_iter)?;
 
         if !payer.is_signer {
           msg!("payer is not a signer");
           return Err(ProgramError::MissingRequiredSignature);
-      }
+       }
+
         let mut user_data = User::try_from_slice(&user.data.borrow())?;
         let mut repo_data = GithubRepo::try_from_slice(&github_repo_account.data.borrow())?;
+        let mut pr_count_data = PrCount::try_from_slice(&pr_count.data.borrow())?;
 
-      // Kullanıcının repo için pull request sayısını kontrol et
-        if let Some(&user_repo_pull_requests) = user_data.pull_requests_per_repo.get(&repo_data.repo_url) {
 
-        if user_repo_pull_requests >= repo_data.pull_request_limit {
+        let (transfer_pda_address, bump) = Pubkey::find_program_address(
+          &[b"transfer",user_data.github_username.as_bytes(),repo_data.repo_url.as_bytes()], 
+          program_id);
+
+        // pr sayisi limitine ulasildi mi?
+        if pr_count_data.prcount >= repo_data.pull_request_limit {
+
           let transfer_amount = repo_data.reward_per_pull_request;
+          
+          let user_wallet_address = Pubkey::new(&user_data.phantom_wallet);
+  
+           // Ödül transfer talimatı
+           let transfer_instruction = system_instruction::transfer(
+            payer.key,
+            &user_wallet_address,
+            transfer_amount,
+           );
+           
+            // Ödül transferini gerçekleştir
+           invoke(
+            &transfer_instruction,
+            &[payer.clone(), user.clone()],
+        )?;
 
-        let (user_pda_address, bump) = Pubkey::find_program_address(
-          &[b"user_pda", payer.key.as_ref()],
-          program_id,
-         );
+        // count guncelle
+           pr_count_data.prcount = pr_count_data.prcount.checked_sub(repo_data.pull_request_limit).ok_or(ProgramError::InvalidAccountData)?;
+        
+      }
+        // pr sayisini arttiralim
+        pr_count_data.prcount = pr_count_data.prcount.checked_add(1).ok_or(InvalidInstruction)?;
 
-         let transfer_instruction = system_instruction::transfer(
-          payer.key,
-          &user_pda_address,
-          transfer_amount,
-         );
-
-      invoke_signed(
-          &transfer_instruction,
-          &[payer.clone(), user.clone()],
-          &[&[b"user_pda", payer.key.as_ref(), &[bump]]],
-      )?;
-
-       msg!("Reward transferred successfully.");
-        } else {
-       msg!("Pull request limit has not been reached.");
-         }
-          } else {
-        msg!("No pull request data found for the given repo.");
-         }
-
+        let mut pr_count_data_account = pr_count.try_borrow_mut_data()?;
+        pr_count_data.serialize(&mut &mut pr_count_data_account[..])?;
+    
          Ok(())
-}
+  }
  
-    // Repo ve PR sayısını goruntule
+    // Hangi repo kac pull request
       pub fn get_pull_requests_per_repo(
       accounts: &[AccountInfo],
      _program_id: &Pubkey,
         ) -> ProgramResult {
      let account_info_iter = &mut accounts.iter();
      let user = next_account_info(account_info_iter)?;
+     let github_repo_account = next_account_info(account_info_iter)?;
+     let pr_count = next_account_info(account_info_iter)?;
 
-    // Kullanıcı verilerini oku
+    // verileri oku
     let user_data = User::try_from_slice(&user.data.borrow())?;
+    let repo_data = GithubRepo::try_from_slice(&github_repo_account.data.borrow())?;
+    let pr_count_data = PrCount::try_from_slice(&pr_count.data.borrow())?;
 
-    // Repo ve PR sayısını yazdır
-   for (repo_url, pr_count) in user_data.pull_requests_per_repo {
-      msg!("Repo: {}, Pull Requests: {}", repo_url, pr_count);
+          msg!(
+            "User: {}, Repo: {}, Pull Requests: {}",
+        user_data.github_username,
+        repo_data.repo_url,
+        pr_count_data.prcount
+          );
+     Ok(())
   }
-
-  Ok(())
-}
  }
-       
+
+     
+     
--- src/state.rs
+++ src/state.rs
@@ -7,32 +7,38 @@ use solana_program::pubkey::Pubkey;
 pub struct User{
     pub github_username: String, 
     pub phantom_wallet:[u8; 32], 
-    pub total_pull_request_count: u32, // kullanicinin yaptigi PR sayisi
-    pub pull_requests_per_repo: HashMap<String, u32>, // Her repo için PR sayısı
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq)]
 pub struct GithubRepo {
     pub repo_url: String, 
     pub total_pull_requests: u32,
-    pub pull_request_limit: u32, 
+    pub pull_request_limit: u64, 
     pub reward_per_pull_request: u64, // Her pull request için ödül miktarı 
     pub owner_wallet_address: [u8; 32],  // repo sahibinin cuzdan adresi
-    pub pull_request_contributors: HashMap<String, u32>, // Her kullanıcı için PR sayısı
 }
 
-
-impl Default for User {
-    fn default() -> Self {
-        User {
-            github_username: String::new(),
-            phantom_wallet: [0; 32],
-            total_pull_request_count: 0,
-            pull_requests_per_repo: HashMap::new(),
-        }
-    }
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq)]
+pub struct PrCount{
+    pub prcount: u64,
 }
 
+// impl Default for User {
+//     fn default() -> Self {
+//         User {
+//             github_username: String::new(),
+//             phantom_wallet: [0; 32],
+//             total_pull_request_count: 0,
+//             pull_requests_per_repo: HashMap::new(),
+//         }
+//     }
+// }
+// count ile pull_request_limit esitse transfer yap
+// esit degilse devam 
+
+// kullancii olusturma
+//yeni repo olusturma
+// transfer yapma
 
 // twitter
 
